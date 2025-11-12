8678f0a1975274d379bd0b33557908ba4f07df58
JMeep2007 <meepegamajulian1@gmail.com>
2025-11-12 12:33:17 -0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by MEEPEGAMA, JULIAN on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
