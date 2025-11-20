ae516727e66b797a4d2abf0868d7ee86358e4a38
JMeep2007 <meepegamajulian1@gmail.com>
2025-11-20 12:37:52 -0800

    11/20/2025

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            
+            Spacer()
+            
+            
+            Button("Press Me!") {
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "You Are Fantastic",
+                                "Fabulous? That's You!",
+                                "You Make Me Smile!",
+                                "When the Genius Bar Needs Help, They Call You!"]
+                
+                message = messages[messageNumber]
+                messageNumber += 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+
+                imageName = "image\(imageNumber)"
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+                
+                print(imageNumber)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            }
         .padding()
+        }
     }
-}
+
 
 #Preview {
     ContentView()
