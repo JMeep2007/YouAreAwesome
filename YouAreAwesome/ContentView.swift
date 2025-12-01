a60155c35859ac0e05668ca931789fe617f91285
JMeep2007 <meepegamajulian1@gmail.com>
2025-12-01 13:02:09 -0800

    12/01/25

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,47 +12,53 @@ struct ContentView: View {
     @State private var imageName = ""
     @State private var imageNumber = 0
     @State private var messageNumber = 0
+    @State private var lastImageNumber = -1
+    @State private var lastMessageNumber = -1
     var body: some View {
         
         VStack {
             Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .animation(.default, value: imageName)
             
             Spacer()
             
-            
-            Button("Press Me!") {
+            Button("Show Message") {
                 let messages = ["You Are Awesome!",
+                                "Gadzooks my friend! I am astonished at how utterly magnificent you are!",
                                 "You Are Great!",
                                 "You Are Fantastic",
                                 "Fabulous? That's You!",
                                 "You Make Me Smile!",
                                 "When the Genius Bar Needs Help, They Call You!"]
                 
+                messageNumber = Int.random(in: 0...messages.count - 1)
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count - 1)
+                } while lastMessageNumber == messageNumber
                 message = messages[messageNumber]
-                messageNumber += 1
-                if messageNumber == messages.count {
-                    messageNumber = 0
-                }
-
+                lastMessageNumber = messageNumber
+                
+                imageNumber = Int.random(in: 0...9)
+                
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while lastImageNumber == imageNumber
                 imageName = "image\(imageNumber)"
-                imageNumber += 1
-                
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
-                
-                print(imageNumber)
+                lastImageNumber = imageNumber
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
